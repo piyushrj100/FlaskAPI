44ab92a691ceb2196c5d04a16f35b86be77cc51b
Piyush Raja <piyush1.bksc@gmail.com>
2022-05-28 22:01:25 +0530

    flask api completed

--- req_test.py
+++ req_test.py
@@ -2,13 +2,13 @@ import requests
 host_url = "http://127.0.0.1:5000"
 # response = requests.get(f"{host_url}/helloworld/Jane")
 # response = requests.post(f"{host_url}/helloworld")
-# data = [{"likes" : 110, "name" : "John", "views" : 100},
-#         {"likes" : 170, "name" : "Jane", "views" : 1100},
-#         {"likes" : 180, "name" : "Michael", "views" : 10900}]
-# for i in range(len(data)) :
-#     response = requests.put(f"{host_url}/video/{str(i)}", data[i]) 
-#     print(response.json())
-# input()
+data = [{"id" : 12, "likes" : 110, "name" : "Harry", "views" : 100},
+        {"id": 70, "likes" : 170, "name" : "Steve", "views" : 1100},
+        {"id": 80, "likes" : 180, "name" : "Bruce", "views" : 10900}]
+for i in range(len(data)) :
+    response = requests.put(f"{host_url}/video/{str(data[i]['id'])}", data[i]) 
+    print(response.json())
+input()
 # response = requests.delete(f"{host_url}/video/0") 
 # print(response.json())
 input()
