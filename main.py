baa0143d8c29884704706186351a652bc617f1d4
Piyush Raja <piyush1.bksc@gmail.com>
2022-05-29 17:17:55 +0530

    functionality for query parameters added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import re
 from flask import Flask, request
 from flask_restful import Api, Resource,reqparse, abort, fields,marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 db = SQLAlchemy(app)
 
@@ -14,7 +16,7 @@ class VideoModel(db.Model) :
     likes=db.Column(db.Integer,nullable=False)
 
     def __repr__(self) :
-        return f"Video(name={name},views={views}, likes={likes})"
+        return f"Video(name={self.name},views={self.views}, likes={self.likes})"
 # db.create_all()  it was used first time to create the model now after every run not needed
 
 
@@ -28,7 +30,10 @@ video_update_args.add_argument("name", type=str, help="Name of the Video is requ
 video_update_args.add_argument("views", type=str, help="views of the Video",location='form')
 video_update_args.add_argument("likes", type=str, help="Likes of the Video", location='form')
 
-
+video_get_args=reqparse.RequestParser()
+#location as args will parse from the query string
+video_get_args.add_argument("name", type=str, help="name required", location='args',required=True)
+video_get_args.add_argument("views", type=str, help="name required",location='args',required=True)
 
 # def abort_if_video_doesnt_exist(video_id) :
 #     if video_id not in videos :
@@ -81,9 +86,7 @@ class Video(Resource) :
             result.likes=args['likes']
         db.session.commit()
         return result
- 
-
-
+    
 
     @marshal_with(resource_fields)
     def  delete(self,video_id) :
@@ -93,10 +96,34 @@ class Video(Resource) :
         db.session.delete(result)
         db.session.commit()
         return '', 204
+
+class video_info(Resource) :
+        
+        @marshal_with(resource_fields)              
+        def get(self) :
+            parsed=video_get_args.parse_args() 
+            result = VideoModel.query.filter_by(name=parsed["name"]).filter_by(views=parsed["views"]).all()
+            if not result :
+                abort(400, message="Not found!!!!")
+            return result
+
+class all_video_info(Resource) :
+    @marshal_with(resource_fields)
+    def get(self) :
+        results = VideoModel.query.all()
+        if not results :
+            abort(404, message="No video exists!!....")
+        return results
+
+
+
+            
         
 
 api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(video_info, "/video")
+api.add_resource(all_video_info,"/video/")
 
 if __name__ == "__main__" :
-    app.run(debug=True)
+    app.run(debug=True) 
 
--- req_test.py
+++ req_test.py
@@ -2,18 +2,22 @@ import requests
 host_url = "http://127.0.0.1:5000"
 # response = requests.get(f"{host_url}/helloworld/Jane")
 # response = requests.post(f"{host_url}/helloworld")
-# data = [{"id" : 12, "likes" : 110, "name" : "Harry", "views" : 100},
-#         {"id": 70, "likes" : 170, "name" : "Steve", "views" : 1100},
-#         {"id": 80, "likes" : 180, "name" : "Bruce", "views" : 10900}]
-# for i in range(len(data)) :
-#     response = requests.put(f"{host_url}/video/{str(data[i]['id'])}", data[i]) 
-#     print(response.json())
+data = [{"id" : 167, "likes" : 12310, "name" : "Suresh", "views" : 456},
+        {"id": 34, "likes" : 19870, "name" : "Piyush", "views" : 37592},
+        {"id": 89, "likes" : 1823, "name" : "Rajan", "views" : 1250}]
+for i in range(len(data)) :
+    response = requests.put(f"{host_url}/video/{str(data[i]['id'])}", data[i]) 
+    print(response.json())
+# # input()
+# # response = requests.delete(f"{host_url}/video/80") 
+# # print(response.json())
 # input()
-response = requests.delete(f"{host_url}/video/80") 
+# response = requests.get(f"{host_url}/video/80") 
 # print(response.json())
-input()
-response = requests.get(f"{host_url}/video/80") 
-print(response.json())
 
-# response = requests.patch(f"{host_url}/video/2", {"views" : 99})
+# # response = requests.patch(f"{host_url}/video/2", {"views" : 99})
+
+
+# # print(response.json())
+# response=requests.get(f"{host_url}/video_info?name=Harry&views=100")
 # print(response.json())
